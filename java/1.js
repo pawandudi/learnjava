2d08b866a6c756c33fc4fe45bd0537c8c2cf91af
pawandudi <pawandudi5000@gmail.com>
2026-06-10 17:47:47 +0530

    second lecture of java

--- java/1.js
+++ java/1.js
@@ -2,3 +2,7 @@ console.log("Hello, World!");
 console.log("Welcome to learning JavaScript.");
  let name="pawan";
 console.log("My name is " + name);
+let age = 20;
+age > 18 ? console.log("Adult") : console.log("Minor");
+let enteredName = prompt("Enter your name: ");
+console.log("Thank you for entering your name: " + enteredName);
