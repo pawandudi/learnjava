a1c9663f3a8697c363f5ec64c292882ae6077a66
pawandudi <pawandudi5000@gmail.com>
2026-06-11 13:02:38 +0530

    3 lecture

--- java/1.js
+++ java/1.js
@@ -6,3 +6,19 @@ let age = 20;
 age > 18 ? console.log("Adult") : console.log("Minor");
 let enteredName = prompt("Enter your name: ");
 console.log("Thank you for entering your name: " + enteredName);
+for (let i = 0; i < 5; i++) {
+    console.log("Iteration: " + i);
+}
+while (age < 25) {
+    console.log("You are still young!");
+    age++;
+}
+do {
+    console.log("This will run at least once.");
+}while (age < 30);
+for ( let i of enteredName) {
+    console.log(i);
+}
+let str = "JavaScript";
+console.log(str.length);
+console.log(str.toUpperCase());
