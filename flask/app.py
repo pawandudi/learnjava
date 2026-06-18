ab390caaf59db6360e593c5ad92e505a1a4622fb
pawandudi <pawandudi5000@gmail.com>
2026-06-18 18:29:51 +0530

    flask/templates/thankyou.html

--- flask/app.py
+++ flask/app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("home.html")
+@app.route("/feedback", methods=["GET", "POST"])
+def feedback():
+    if request.method == "POST":
+        name = request.form.get("username")
+        message = request.form.get("message")
+        return render_template("thankyou.html", name = name, message=message)
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+    return render_template("feedback.html")
